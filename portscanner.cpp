53bc53e2d371588d798b3ba10116fdef23a96c66
Anthony Hicks <ajhicks94@gmail.com>
2018-12-05 12:25:32 -0600

    add port regexes

--- portscanner.cpp
+++ portscanner.cpp
@@ -35,14 +35,15 @@ void printUsage()
     cout << "                      accepted formats:" << endl;
     cout << "                          single port:    --port 23" << endl;
     cout << "                          range of ports: --port 2000-3000" << endl;
-    cout << "                          list of ports:  --port 21, 80, 443" << endl;
+    cout << "                          list of ports:  --port 21,80,443" << endl;
+    cout << "                                          NOTE: no space between values and delimiters" << endl;
     cout << "          --ip" << endl;
     cout << "                      scan designated ip address" << endl;
     cout << "                      accepted formats:" << endl;
     cout << "                          single ip:      --ip 129.120.93.94" << endl;
     cout << "                          range of ips:   --ip 129.120.93.87-200" << endl;
     cout << "                          list of ips:    --ip 129.120.95.101,129.121.96.40" << endl;
-    cout << "                                          NOTE: no spaces between values and commas" << endl;
+    cout << "                                          NOTE: no space between values and delimiters" << endl;
     cout << "          --file" << endl;
     cout << "                      scan ip addresses in file" << endl;
     cout << "          --transport" << endl;
@@ -62,21 +63,38 @@ int parseOptions(int argc, char* argv[])
     vector<string> protocols;
 
     cmatch match;
+
+    // Matches a single "port" "0", "99999", we bounds check later
+    regex re_port ("\\b([0-9]{1,5})\\b");
+
+    // Matches a port range "0-0", "5000-2122"
+    regex re_port_range ("\\b([0-9]{1,5})-([0-9]{1,5})\\b");
+
+    // Matches one port followed by a comma followed by a digit followed by anything
+    regex re_port_list ("\\b([0-9]{1,5})\\b,\\d.*");
+
+    // Matches a single ip address
     regex re_ip ("\\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
     "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
     "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
     "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\b");
 
+    // Matches a range of ip addresses on the same subnet
+    // 129.120.93.87-95
     regex re_ip_range ("\\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
     "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
     "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
     "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])-(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\b");
 
+    // Matches one ip addresses followed by a comma followed by a digit followed by anything
+    // 129.120.93.85,1
+    // Just used to determine the mode of parsing
     regex re_ip_list ("\\b(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
     "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
     "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\."
     "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\b,\\d.*");
 
+    // Loop through all the arguments
     for(size_t i = 1; i < argc; i++)
     {
         if(strcmp(argv[i], "--help") == 0)
@@ -87,6 +105,35 @@ int parseOptions(int argc, char* argv[])
         else if(strcmp(argv[i], "--port") == 0)
         {
             // add ports to port list
+            if(i + 1 != argc)
+            {
+                // add list of ports
+                if()
+                {
+
+                }
+                // add range of ports
+                else if()
+                {
+
+                }
+                // add single port
+                else if()
+                {
+
+                }
+                else
+                {
+                    cout << "invalid port: " << argv[i+1] << endl;
+                    exit(1);
+                }
+
+            }
+            else
+            {
+                cout << "missing arg: " << argv[i] << endl;
+                exit(1);
+            }
             // or add port range to port list (decode range)
         }
         else if(strcmp(argv[i], "--ip") == 0)
@@ -95,7 +142,7 @@ int parseOptions(int argc, char* argv[])
             {
                 if(i + 1 != argc)
                 {
-                    // matches list of ips (well, it would match this 129.120.210.235 , 2)
+                    // matches list of ips
                     if(regex_match(argv[i+1], re_ip_list))
                     {
                         // Split the argument by commas
@@ -240,7 +287,7 @@ int parseOptions(int argc, char* argv[])
     // Check for any duplicate ips in ip list and remove them
     sort(ips.begin(), ips.end());
     ips.erase(unique(ips.begin(), ips.end()), ips.end());
-    
+
     // Check to see if anything integral wasn't assigned
     // If so, assign defaults here and return object? json?
     
